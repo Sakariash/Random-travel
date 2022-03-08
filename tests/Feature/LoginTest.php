5bd689a4705eba3c7a697c5049aa025061504345
Sakariash <simonsakarias@gmail.com>
2022-03-08 22:57:38 +0100

    LoginTest done

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_view_login_form()
+    {
+        $response = $this->get('/');
+        $response->assertSeeText('Email');
+        $response->assertStatus(200);
+    }
+
+    public function test_login_user()
+    {
+        $user = new User();
+        $user->name = 'tester';
+        $user->email = 'test@test.com';
+        $user->password = Hash::make('123');
+        $user->save();
+
+        $response = $this
+            ->followingRedirects()
+            ->post('login', [
+                'email' => 'test@test.com',
+                'password' => '123',
+            ]);
+        $response->assertSeeText('Hello, tester. Would you like to logout?');
+    }
+
+    public function test_login_user_without_password()
+    {
+        $response = $this
+            ->followingRedirects()
+            ->post('login', [
+                'email' => 'test@test.com',
+            ]);
+
+        $response->assertSeeText('Whoops! Please try to login again.');
+    }
+}
