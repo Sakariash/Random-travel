25e1e8cdee858d57f116a1eba53dd57c8871fcda
Sakariash <simonsakarias@gmail.com>
2022-03-10 15:36:50 +0100

    lint fix

--- app/Http/Controllers/RandomController.php
+++ app/Http/Controllers/RandomController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Continent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use illuminate\Support\Facades\DB;
-
 
 class RandomController extends Controller
 {
@@ -18,33 +18,20 @@ class RandomController extends Controller
     public function __invoke(Request $request)
     {
         $continent = $request->get('continent');
-        // dd($request->get('continent'));
-        // $continent =  $request->get('continent');
-        //Select id where continent = continent
-        //Select all countries where continent-id = id
-        //random country return
 
-        $countryID = DB::table('continents')
-            ->SELECT('id')
-            ->WHERE('continent', $continent)
-            ->get();
 
-        $random = DB::table('countries')
-            ->SELECT("*")
-            ->WHERE('id', "=", $countryID)
-            ->random()
-            ->get();
+        foreach (Continent::all() as $conti) {
+            var_dump($conti['continent']);
+        }
 
-        return view('dashboard', [
-            'randomCountry' => $random,
-        ]);
+        // $getContinent = Continent::where('id', '2')
+        //     ->get();
 
-        // $user = Auth::user();
-
-        // $countries = DB::table('countries')->get('country');
-        // return view('dashboard', [
-        //     'user' => $user,
-        //     'countries' => $countries,
-        // ]);
+        // dd($getContinent);
     }
+
+
+    // foreach (Continent::all() as $continent) {
+    //     echo $continent->name;
+    // }
 }
