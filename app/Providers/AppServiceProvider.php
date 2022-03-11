0e3effbf0cdf4cdd365c677d123426101930ae9a
Sakariash <simonsakarias@gmail.com>
2022-03-11 11:55:12 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Continent;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $continents = Continent::all();
+
+        view()->share('continents', $continents);
     }
 }
