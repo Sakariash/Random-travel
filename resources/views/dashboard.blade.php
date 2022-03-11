8bdba79959e17b57356ff14c8ed0f80fe6032eef
Sakariash <simonsakarias@gmail.com>
2022-03-11 11:55:05 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,20 +4,32 @@
 </p>
 @endif
 
-
-
-<form method="post" action="random">
+<!-- <form method="get" action="random">
     @csrf
     <label for="name">continent</label>
     <select name="continent" id="continent" type="text">
         @foreach($continents as $key => $value)
-        <option value={{$value->continent}}>{{$value->continent}}</option>
+        <option value={{$value->id}}>{{$value->continent}}</option>
         @endforeach
     </select>
     <button>selcect</button>
-</form>
+</form> -->
+
+@foreach($continents as $key => $value)
+<div>
+    <a href="/random/{{$value->continent}}">{{$value->continent}}
+</div></a>
+@endforeach
+
+<br>
+@if(isset($country))
+{{$continent->continent}}--
+<a href="/random/{{$continent->continent}}/{{$country}}">{{$country}}</a>
+@endif
+
+<!-- add favourite / travel -->
 
 
 <p>
-    Hello, {{$user->name}}.<br><br> Would you like to <a href="logout">logout?</a>
+    Hello, {{auth()->user()->name}}.<br><br> Would you like to <a href="/logout">logout?</a>
 </p>
