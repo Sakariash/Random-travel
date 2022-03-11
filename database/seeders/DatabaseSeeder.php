ad2f6ecab3e9070e7b409c22d7469c49d76fd5dd
Sakariash <simonsakarias@gmail.com>
2022-03-11 15:24:46 +0100

    factory/seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Continent;
+use App\Models\Country;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,13 +19,149 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Continent::create([
-            'continent' => 'Asia',
-            'continent' => 'Africa',
-            'continent' => 'Europe',
-            'continent' => 'North America',
-            'continent' => 'South America',
-            'continent' => 'Oceania/Australia',
-        ]);
+
+        $continents = [
+            new Continent(['continent' => 'Asia']),
+            new Continent(['continent' => 'Africa']),
+            new Continent(['continent' => 'Europe']),
+            new Continent(['continent' => 'North America']),
+            new Continent(['continent' => 'South America']),
+            new Continent(['continent' => 'Oceania/Australia']),
+        ];
+
+        foreach ($continents as $continent) {
+            $continent->save();
+        }
+
+        $countries = [
+            new Country([
+                'country' => 'Uzbekistan',
+                'continent_id' => 1
+            ]),
+            new Country([
+                'country' => 'Saudi Arabia',
+                'continent_id' => 1
+            ]),
+            new Country([
+                'country' => 'Afghanistan',
+                'continent_id' => 1
+            ]),
+            new Country([
+                'country' => 'Iraq',
+                'continent_id' => 1
+            ]),
+            new Country([
+                'country' => 'Algeria',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'Uganda',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'Kenya',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'South Africa',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'Tanzania',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'Uganda',
+                'continent_id' => 2
+            ]),
+            new Country([
+                'country' => 'Algeria',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'United Kingdom',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'France',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'Italy',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'Spain',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'Ukraine',
+                'continent_id' => 3
+            ]),
+            new Country([
+                'country' => 'Jamaica',
+                'continent_id' => 4
+            ]),
+            new Country([
+                'country' => 'Puerto Rico',
+                'continent_id' => 4
+            ]),
+            new Country([
+                'country' => 'Panama',
+                'continent_id' => 4
+            ]),
+            new Country([
+                'country' => 'Costa Rica',
+                'continent_id' => 4
+            ]),
+            new Country([
+                'country' => 'Bahamas',
+                'continent_id' => 4
+            ]),
+            new Country([
+                'country' => 'Venezuela',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Peru',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Argentina',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Colombia',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Brazil',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Australia',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Papua New Guinea',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'New Zealand',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Fiji',
+                'continent_id' => 5
+            ]),
+            new Country([
+                'country' => 'Solomon Islands',
+                'continent_id' => 5
+            ]),
+        ];
+
+        foreach ($countries as $country) {
+            $country->save();
+        }
     }
 }
