40c81b8fea37eb14437ca8158446507d661556f9
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-03-11 17:02:53 +0100

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,16 +1,15 @@
 <form method="post" action="register">
     @csrf
-    <div>
-        <label for="name">Name</label>
-        <input name="name" id="name" type="text" required />
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input name="email" id="email" type="email" required />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password" required />
-    </div>
+
+    <label for="name">Name</label>
+    <input name="name" id="name" type="text" required />
+
+
+    <label for="email">Email</label>
+    <input name="email" id="email" type="email" required />
+
+    <label for="password">Password</label>
+    <input name="password" id="password" type="password" required />
+
     <button type="submit">Register</button>
 </form>
