dfa7b41394912001c6ce690900fdc9043e06a81e
Sakariash <simonsakarias@gmail.com>
2022-03-14 18:12:22 +0100

    Routes updated, single page work now patrik..... :)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RandomController;
+use App\Http\Controllers\SearchController;
 use App\Models\Continent;
 use App\Models\Country;
 use Illuminate\Support\Facades\Route;
@@ -28,7 +29,7 @@ Route::view('/', 'index')->name('login')->middleware('guest');
 Route::view('register', 'register');
 Route::post('register', RegisterController::class);
 Route::post('login', LoginController::class);
-Route::get('random/{continent:continent}', RandomController::class);
+Route::get('random/{continent:continent}', RandomController::class)->name('random.country');
 
 
 Route::post('/random/check/{country:country}', TripController::class);
@@ -38,6 +39,7 @@ Route::get('random/{continent:continent}/{country:country}', function (Continent
 });
 
 Route::get('logout', LogoutController::class);
-Route::get('dashboard', DashboardController::class)->middleware('auth');
+Route::get('dashboard', DashboardController::class)->name('dashboard')->middleware('auth');
 
 Route::view('trips', 'trips');
+Route::get('/search', SearchController::class);
