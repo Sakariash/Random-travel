16282be5c0c9b265aec7771caf1ef17df3cf6a51
Sakariash <simonsakarias@gmail.com>
2022-03-15 16:12:40 +0100

    Random test added

--- tests/Feature/RandomTest.php
+++ tests/Feature/RandomTest.php
@@ -2,35 +2,41 @@
 
 namespace Tests\Feature;
 
+use App\Models\Continent;
 use App\Models\User;
+use Database\Factories\ContinentFactory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RandomTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
-    public function test_get_random_country()
+    public function test_random_country_link()
     {
+        $this->seed();
         $user = new User();
-        $user->name = 'Jennifer';
-        $user->email = 'jenn@jenn.se';
+        $user->name = $this->faker->name();
+        $user->email = $this->faker->email();
         $user->password = Hash::make('123');
         $user->save();
 
         $response = $this
             ->followingRedirects()
             ->post('login', [
-                'email' => 'jenn@jenn.se',
+                'email' => $user->email,
                 'password' => '123',
             ]);
 
+        $continent = Continent::first();
+
         $response = $this
             ->actingAs($user)
-            ->get('random/{continent:continent}', [
-                'name' => 'random.country',
-            ]);
+            ->get("random/{$continent->continent}");
+
         $response->assertOk();
     }
 }
